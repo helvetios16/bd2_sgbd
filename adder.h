8f49614b5eb651e575a9e49d3ebb577cfc172b71
helvetios16 <smendozafernandez437@gmail.com>
2024-04-21 21:26:47 -0500

    The function of adding records without using vector is now complete

--- adder.h
+++ adder.h
@@ -3,7 +3,10 @@
 class Adder {
    public:
     void addScheme(const std::string& archive, const std::string& variable);
-    void addRegister(std::string archive);
+    void addRegister(const std::string& archive, const std::string& variable);
     bool validType(const std::string& type);
     bool convertToNumber(const std::string& number);
+    int sizeString(const std::string& line, const char& symbol);
+    bool checkType(const std::string& type, const std::string& variable);
+    bool convertToNumber(const std::string& number, const std::string& type);
 };
--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 
 int main(int argc, char const *argv[]) {
     Adder adison;
-    adison.addScheme("prueba", "nombre,string,edad,int,hola,hola");
+    // adison.addScheme("prueba", "nombre,string,edad,int");
+    // adison.addRegister("pruesa", "sebas,19");
+    // adison.addScheme("index", "name,string,age,int,student,bool");
+    // adison.addRegister("index", "sebas,19,1");
     return 0;
 }
