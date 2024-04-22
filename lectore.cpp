611faab433457e1e4569144888668e5a52f36847
helvetios16 <smendozafernandez437@gmail.com>
2024-04-21 22:17:41 -0500

    You can now check if the CSV has already been read

--- lectore.cpp
+++ lectore.cpp
@@ -6,12 +6,22 @@
 #include <string>
 
 void Lectore::readCsv(const std::string& csv, const std::string& variable) {
+    // separar el punto csv
     size_t pos = csv.rfind('.');
     std::string archive;
     if (pos != std::string::npos) {
         archive = csv.substr(0, pos);
     }
-    std::cout << archive << std::endl;
+    // comprovar que no este en el equema es csv
+    std::string searchLine = searchSheme(archive);
+    if (archive == "") {
+        std::cout << "El archivo csv ya se ha leido" << std::endl;
+        return;
+    }
+    // punto
+    // std::cout << archive << std::endl;
+    // pasar los datos de las columas al esquema
+    // pasar los datos del csv a su archivo txt
 }
 
 std::string Lectore::searchSheme(const std::string& archive) {
