45a95114df83919f385d5f73db309e9517ab7029
helvetios16 <smendozafernandez437@gmail.com>
2024-04-29 19:44:36 -0500

    Proof

--- main.cpp
+++ main.cpp
@@ -4,15 +4,18 @@
 
 int main(int argc, char const *argv[]) {
     SGBD sgbd;
+    sgbd.createDatabase("esquema");
+    sgbd.useDatabase("esquema");
+    sgbd.createTable("edades");
+    // sgbd.addColumn("nombre string", "edades");
+    sgbd.showtable("edades");
     // sgbd.shell();
     // sgbd.see("edades", "*", "", "");
-    sgbd.useDatabase("scheme");
     // sgbd.addCsvToTable("Titanic.csv", "Titanic");
-    sgbd.showtable("otro_titanic");
-    sgbd.see("otro_titanic", "PassengerId,Name", "", "");
+    // sgbd.showtable("otro_titanic");
+    // sgbd.see("otro_titanic", "PassengerId,Name", "", "");
     // sgbd.createTable("Hola");
     // sgbd.addColumn("nombre string", "Hola");
-    // sgbd.createDatabase("scheme");
     // sgbd.useDatabase("scheme");
     return 0;
 }
